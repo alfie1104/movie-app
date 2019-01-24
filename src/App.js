d3820579665dc1944865900b32145e4266b648db
peter_alfie <alfie1104@gmail.com>
2019-01-24 13:27:58 +0900

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movie from "./Movie";
 
 class App extends Component {
+  state = {
+    greeting: 'Hello!',
+  };
+
+  componentWillMount() {
+
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          {
+            title: "Matrix",
+            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/0/06/Ultimate_Matrix_Collection_poster.jpg/220px-Ultimate_Matrix_Collection_poster.jpg"
+          },
+          {
+            title: "Full Metal Jacket",
+            poster: "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2017/05/full_metal_jacket_1987_3-h_2017.jpg",
+          },
+          {
+            title: "Oldboy",
+            poster: "https://cdn1.thr.com/sites/default/files/imagecache/landscape_928x523/2013/11/oldboy-still.jpg",
+          },
+          {
+            title: "Star Wars",
+            poster: "https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png"
+          },
+          {
+            title: "Trainspoting",
+            poster: "https://lumiere-a.akamaihd.net/v1/images/og-generic_02031d2b.png"
+          }
+        ]
+      })
+    }, 5000);
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => (
+      <Movie key={index} title={movie.title} poster={movie.poster} />
+    ));
+
+    return movies;
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.greeting}
+        {this.state.movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
